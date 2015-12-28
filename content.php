677b61a56f2896c67ded4a62f4c6e387a404bbbd
Thomas Worofsky <Thomas.Worofsky@gmail.com>
2015-12-28 13:21:31 +0100

    Fix Content

--- content.php
+++ content.php
@@ -1,3 +1,3 @@
-<?php get_header(); ?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
-<?php get_footer(); ?>
+</article>
