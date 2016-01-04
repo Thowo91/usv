76bb168fa0fb66b57b5e39baf5e8702e3feb2c4a
Thomas Worofsky <Thomas.Worofsky@gmail.com>
2016-01-04 17:36:11 +0100

    Add Pagination Shortcode usage

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php get_header(); ?>
-<?php while (have_posts()) : the_post(); ?>
-    <?php get_template_part('content', get_post_format()); ?>
+<?php while ( have_posts() ) : the_post(); ?>
+	<?php get_template_part( 'content', get_post_format() ); ?>
 <?php endwhile; ?>
+<?php
+if ( function_exists( usv_pagination_shortcode ) ) {
+	echo do_shortcode( '[pagination]' );
+}
+?>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
